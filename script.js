998d056f53a4b917a59a5c6f2e7160677fe60d00
Štěpán Němeček <nemecekstepan246@student.voskh.cz>
2025-10-23 13:26:55 +0200

    Přidání kliknutí na obrázek

---
+++ script.js
@@ -0,0 +1,8 @@
+const h1Jmeno = document.getElementById("h1Jmeno");
+const imgGender = document.getElementById("imgGender");
+
+imgGender.width = "250"
+
+function changeGender () {
+    imgGender.src = "obrazky/dite-zena.png";
+}
