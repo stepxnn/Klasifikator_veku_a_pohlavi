95111c5a6c9c716cca84f5e4efc5ad56f32e0809
Štěpán Němeček <nemecekstepan246@student.voskh.cz>
2025-10-23 13:33:54 +0200

    Přidání funkčního scriptu překlikávání mezi obrázky

--- script.js
+++ script.js
@@ -3,6 +3,14 @@ const imgGender = document.getElementById("imgGender");
 
 imgGender.width = "250"
 
+let pocitadlo = 1;
+
 function changeGender () {
+    if (pocitadlo % 2 == 0) {
+    imgGender.src = "obrazky/dite-muz.png";
+    }
+    else {
     imgGender.src = "obrazky/dite-zena.png";
+    }
+  pocitadlo = pocitadlo + 1;
 }
