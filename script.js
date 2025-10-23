3b688b30a0ecf47f1677c4270f947095e2f43a8c
Štěpán Němeček <nemecekstepan246@student.voskh.cz>
2025-10-23 13:49:20 +0200

    Funkční script změna fotky i jména

--- script.js
+++ script.js
@@ -8,9 +8,12 @@ let pocitadlo = 1;
 function changeGender () {
     if (pocitadlo % 2 == 0) {
     imgGender.src = "obrazky/dite-muz.png";
+    h1Jmeno.textContent = "Štěpán";
     }
     else {
+    h1Jmeno.textContent = "Štěpánka";
     imgGender.src = "obrazky/dite-zena.png";
     }
-  pocitadlo = pocitadlo + 1;
+  //pocitadlo = pocitadlo + 1;
+    pocitadlo++; //inkrementuj (přičti o 1)
 }
